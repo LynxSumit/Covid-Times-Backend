596dd8a054894b0a4024fba253acc7ed0e959c72
Lynx-Sumit <skumarupadhyay1@gmail.com>
2023-08-14 17:39:19 +0530

    cc

--- index.js
+++ index.js
@@ -43,8 +43,8 @@ app.post("/contact", async (req,res)=>{
         }
     })
     const mailOptions = {
-        from : process.env.EMAIL,
-        to : email,
+        from : email,
+        to : process.env.EMAIL,
         subject : `Hey Vijay ${name} wants to talk to you `,
         html : `<h4>name : ${name} <br> email : ${email} <br> phone : ${phone} <br> Address : ${address} </h4><br><br><p>${message}</p>`,
 
